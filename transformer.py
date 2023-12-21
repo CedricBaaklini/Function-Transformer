a89170887e4fdc75cee9415d80debbcf08784d1a
Cedric Baaklini <cabaaklini@icloud.com>
2023-12-20 23:37:14 -0500

    Adding files

---
+++ transformer.py
@@ -0,0 +1,7 @@
+class Transformer:
+
+    def __init__(self, a, b, c, d):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
---
+++ transformer.ts
@@ -0,0 +1,15 @@
+class Transformer {
+    a: number;
+    b: number;
+    c: number;
+    d: number;
+
+    constructor(a: number, b: number, c: number, d: number) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
+}
+
+console.log("Hello there");
